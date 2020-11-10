0ea3c9e6c2f2e1c055264f197398d24c84a8e819
Adriel <adrielfalcao@gmail.com>
2020-11-10 11:04:47 -0300

    Cleaning files

--- Voice_Pre_Processing/reading_wav.py
+++
@@ -1,9 +0,0 @@
-from scipy.io import wavfile
-# if you're on Windows
-import winsound
-
-filename = 'Data/microphone-results.wav'
-winsound.PlaySound(filename, winsound.SND_FILENAME)
-
-filename1 = 'Data/clean_microphone.wav'
-winsound.PlaySound(filename1, winsound.SND_FILENAME)
